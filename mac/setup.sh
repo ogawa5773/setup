1b56b9b067cacb566b7843f54d2c31179c6800d9
daichi <daichi@daichinoMacBook-Pro-2.local>
2022-10-19 11:34:16 +0900

    remove windouws_setup_script

--- mac/setup.sh
+++ mac/setup.sh
@@ -9,43 +9,44 @@ brew install --cask slack
 brew install --cask kindle
 # https://qiita.com/rmlabo/items/c181b6b402a4b860fcc6
 
+### zsh 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 ### Ruby
 brew install rbenv ruby-build
-rbenv init
-source ~/.bash_profile
-# https://github.com/rbenv/rbenv
+eval "$(rbenv init - zsh)"
 
 ### Python
 brew install pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
-# https://github.com/pyenv/pyenv
-
-### Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-# https://www.rust-lang.org/tools/install
+echo 'eval "$(pyenv init --path)"' >> ~/.zshrc
 
 ### Node.js/TypeScript
 curl https://get.volta.sh | bash
+source ~/.zshrc
 volta install node
 npm install --global typescript
-# https://volta.sh/, https://www.typescriptlang.org/download
+# https://volta.sh/ https://www.typescriptlang.org/download
 
 ### Terraform
 brew install tfenv
-# https://github.com/tfutils/tfenv
 
 ### AWS
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg AWSCLIV2.pkg -target /
+rm AWSCLIV2.pkg
 # https://docs.aws.amazon.com/ja_jp/cli/latest/userguide/getting-started-install.html
 
 ### Kubernetes
 brew install kubectl
-# https://kubernetes.io/ja/docs/tasks/tools/install-kubectl/
 
 ### MySQL
-# brew install mysql
+brew install mysql
 # https://prog-8.com/docs/mysql-env
+
+### DockerSync 
+brew install eugenmayer/dockersync/unox
+gem install --user-install docker-sync
+# vim ~/.zshrc
+# if which ruby >/dev/null && which gem >/dev/null; then
+#   PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
+# fi
