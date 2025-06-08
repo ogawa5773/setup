28fb242b8d380155925d9c0f4516412751a20093
ogawa5773 <anaminkumamoto@gmail.com>
2025-06-08 13:08:19 +0900

    202506081308

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,19 +1,23 @@
+# Homebrewのインストール
 brew update
 
 ### Desktop App
-brew install --cask docker
-brew install --cask visual-studio-code
-brew install --cask tableplus
-brew install --cask postman
+# https://formulae.brew.sh/
 brew install --cask bitwarden
-brew install --cask brave-browser
-brew install --cask google-chrome
+brew install --cask docker
+brew install --cask claude
+brew install --cask cursor
+brew install --cask figma
+brew install --cask twingate
+brew install --cask sequel-ace
 brew install --cask slack
 brew install --cask discord
-brew install --cask kindle
+brew install --cask brave-browser
+brew install --cask notunes
 brew install --cask cmd-eikana
-
-# https://formulae.brew.sh/
+# 手動インストール
+# flow
+# TATEditor
 
 ### zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -53,20 +57,10 @@ brew install weaveworks/tap/eksctl
 brew install mysql
 # https://prog-8.com/docs/mysql-env
 
-### DockerSync 
-brew install eugenmayer/dockersync/unox
-gem install --user-install docker-sync
-# vim ~/.zshrc
-# if which ruby >/dev/null && which gem >/dev/null; then
-#   PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
-# fi
-
-
-
-# TablePlusのインポート
-# ブラウザbookmarkのインポート
-# AWS CLIの設定
-# kubectlの設定
-# https://www.google.com/search?q=kubectl+config+eks&ei=bMpPY4vtMZCi-QaZ-5bwAw&ved=0ahUKEwiL04uUhOz6AhUQUd4KHZm9BT4Q4dUDCA8&uact=5&oq=kubectl+config+eks&gs_lcp=Cgdnd3Mtd2l6EAMyBwgAEIAEEBMyCAgAEAgQHhATMggIABAIEB4QEzIICAAQCBAeEBMyCAgAEAgQHhATOgoIABBHENYEELADOgUIABCABDoECAAQHjoGCAAQCBAeOgYIABAFEB46DggAEOoCELQCENkCEOUCOgsIABCPARCPARDqAjoECAAQQzoJCAAQBBCABBADOg0IABAEEIAEELEDEIMBOgcIABAEEIAEOgoIABAEEIAEELEDOgoIABCxAxCDARBDOgsIABCABBCxAxCDAToICAAQHhAKEBM6BggAEB4QEzoKCAAQCBAeEAoQEzoHCAAQgAQQDUoECEEYAEoECEYYAFCgBVjgV2DMWWgFcAB4AoABwAGIAcAfkgEEMzMuOZgBAKABAbABCsgBCsABAQ&sclient=gws-wiz
-# キーボード設定
-# https://qiita.com/suin/items/33d0f60fcd3c01509c44
+### その他
+# ~/.zshrcの複製
+# ~/.aws/*の複製
+# ブラウザのブックマークインポート
+# SequelAceのインポート
+# kubectlの設定（https://www.google.com/search?q=kubectl+config+eks&ei=bMpPY4vtMZCi-QaZ-5bwAw&ved=0ahUKEwiL04uUhOz6AhUQUd4KHZm9BT4Q4dUDCA8&uact=5&oq=kubectl+config+eks&gs_lcp=Cgdnd3Mtd2l6EAMyBwgAEIAEEBMyCAgAEAgQHhATMggIABAIEB4QEzIICAAQCBAeEBMyCAgAEAgQHhATOgoIABBHENYEELADOgUIABCABDoECAAQHjoGCAAQCBAeOgYIABAFEB46DggAEOoCELQCENkCEOUCOgsIABCPARCPARDqAjoECAAQQzoJCAAQBBCABBADOg0IABAEEIAEELEDEIMBOgcIABAEEIAEOgoIABAEEIAEELEDOgoIABCxAxCDARBDOgsIABCABBCxAxCDAToICAAQHhAKEBM6BggAEB4QEzoKCAAQCBAeEAoQEzoHCAAQgAQQDUoECEEYAEoECEYYAFCgBVjgV2DMWWgFcAB4AoABwAGIAcAfkgEEMzMuOZgBAKABAbABCsgBCsABAQ&sclient=gws-wiz
+# キーボード設定（https://qiita.com/suin/items/33d0f60fcd3c01509c44
